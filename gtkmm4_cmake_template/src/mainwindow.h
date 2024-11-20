6b44bdfc5fe0f89a7a7c216c2422b0c027c785ec
Anthony Irwin <git@anthonyirwin.com>
2024-11-20 13:19:22 +1000

    Added gtkmm4 template

---
+++ gtkmm4_cmake_template/src/mainwindow.h
@@ -0,0 +1,22 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <gtkmm/button.h>
+#include <gtkmm/window.h>
+
+class MainWindow : public Gtk::Window
+{
+
+public:
+  MainWindow();
+  ~MainWindow() override;
+
+protected:
+  //Signal handlers:
+  void on_button_clicked();
+
+  //Member widgets:
+  Gtk::Button m_button;
+};
+
+#endif
